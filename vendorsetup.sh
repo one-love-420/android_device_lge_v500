cae5337eee4954f5723b846d5365f43bca74ea9b
one-love-420 <johns6md@gmail.com>
2014-07-16 21:07:33 -0400

    Updated device v500 for Mahdi-Rom

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 #
-# Copyright 2012 The Android Open Source Project
+# Copyright 2013 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,6 +14,4 @@
 # limitations under the License.
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/v500.mk \
-    $(LOCAL_DIR)/mahdi.mk
+add_lunch_combo mahdi_v500-userdebug
