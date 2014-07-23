86f0cbcc9467c76514029cee581d7adb85ab16fb
one-love-420 <johns6md@gmail.com>
2014-07-22 23:45:04 -0400

    Update build fingerprint to KOT49I

--- mahdi.mk
+++ mahdi.mk
@@ -15,7 +15,7 @@ PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-V500
 PRODUCT_MANUFACTURER := LGE
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=awifi_open_eu BUILD_FINGERPRINT=lge/awifi_open_eu/awifi:4.2.2/JDQ39B/V50010a.1380629540:user/release-keys PRIVATE_BUILD_DESC="awifi_open_eu-user 4.2.2 JDQ39B V50010a.1380629540 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=awifi_open_eu BUILD_FINGERPRINT=lge/awifi_open_eu/awifi:4.4.2/KOT49I.V50020d/.1401936573:user/release-keys PRIVATE_BUILD_DESC="awifi_open_eu-user 4.4.2 KOT49I.V50020d .1401936573 release-keys"
 
 # Copy device specific prebuilt files.
 PRODUCT_COPY_FILES += \
