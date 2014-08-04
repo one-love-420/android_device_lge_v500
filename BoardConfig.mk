709e2b19286b1f93a172996778054b5c755b72d9
one-love-420 <johns6md@gmail.com>
2014-08-03 22:30:03 -0400

    Remove duplicate entry from BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -130,8 +130,6 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
 TARGET_RECOVERY_FSTAB = device/lge/v500/fstab.gvar
 
-BOARD_HAS_NO_SELECT_BUTTON := true
-
 BOARD_SEPOLICY_DIRS += \
         device/lge/v500/sepolicy
 
